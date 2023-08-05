251e5bcba470719056aa82d84bdcd9962bf1b3c6
Alex Coronell <alexcoronell@gmail.com>
2023-08-05 14:06:28 -0500

    Basic load tasks in home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,20 @@
-function HomePage() {
+import { connectDB } from "@/utils/monsoose";
+import Task from "@/models/Task";
+import TaskCard from "@/components/TaskCard";
+
+async function loadTasks() {
+  connectDB()
+  return await Task.find()
+}
+
+async function HomePage() {
+  const tasks = await loadTasks()
   return (
-    <div>HomePage</div>
+    <div className="grid grid-cols-3 gap-2">
+      {tasks.map(task => (
+        <TaskCard task={task} key={task.id} />
+      ))}
+    </div>
   )
 }
 
